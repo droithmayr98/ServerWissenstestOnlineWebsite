39230cb3c06f59bab0667474708271c4843b1cbd
Manuel <schloga99@gmx.at>
2017-10-05 20:40:45 +0200

    Add project files.

---
+++ WissenstestOnline/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WissenstestOnline.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult AdminOverview() {
+            return View();
+        }
+
+        public IActionResult CreateNewAdmin() {
+            return View();
+        }
+
+    }
+}
---
+++ WissenstestOnline/Controllers/MainController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WissenstestOnline.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Start()
+        {
+            return View();
+        }
+
+        public IActionResult SelectStation() {
+            return View();
+        }
+
+        public IActionResult Info() {
+            return View();
+        }
+
+        public IActionResult Kontakt() {
+            return View();
+        }
+
+
+
+    }
+}
