92781f7a1b99cdb7b7eaf5896fdc7bbe50fab76c
lhettmann <luk.hettmann@gmx.at>
2017-11-30 15:04:55 +0100

    Adminmodus angefangen

--- WissenstestOnline/Controllers/AdminController.cs
+++ WissenstestOnline/Controllers/AdminController.cs
@@ -4,43 +4,68 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WissenstestOnline.Models;
+using DB_lib;
+using Microsoft.Extensions.Logging;
+using DB_lib.Tables;
 
 namespace WissenstestOnline.Controllers
 {
-    public class AdminController : Controller     
+    public class AdminController : Controller
     {
-        [HttpPost]
-        public IActionResult Login(Admin admin)
+
+        private TestDB_Context test_db;
+        private ILogger<MainController> logger;
+
+        public AdminController(TestDB_Context db, ILogger<MainController> logger)
         {
-            
-            if (ModelState.IsValid)
-            {
-               if( admin.IsValid(admin.UserName, admin.Password))
-                {
-                    return RedirectToAction("AdminOverview", "Admin");
-                }
-                else
-                {
-                    ModelState.AddModelError("", "Login data is incorrect!");
-                }
-            }
-
-          return View(admin);
+            this.test_db = db;
+            this.logger = logger;
         }
 
-        public IActionResult AdminOverview() {
+
+        public IActionResult Login()
+        {
+            logger.LogInformation("Testlog Admincontroller");
             return View();
         }
 
-        public IActionResult CreateNewAdmin() {
+        public IActionResult AdminOverview()
+        {
             return View();
         }
-        
+
+        public IActionResult CreateNewAdmin()
+        {
+            return View();
+        }
+
         public IActionResult Logout()
         {
-            
+
             return RedirectToAction("Start", "Main");
         }
+
+        //Ajax calls
+
+        public string CheckAdminInfo(string username, string password) {
+
+            Admintable admin =  test_db.Admins.SingleOrDefault(x => x.Username.Equals(username));
+            if (admin == null)
+            {
+                return "username_fail";
+            }
+            else if (!admin.Password.Equals(password))
+            {
+                return "password_fail";
+            }
+            else {
+                return "ok";
+            }
+        }
+
+
+
+
 
     }
 }
---
+++ WissenstestOnline/wwwroot/js/admin.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(() => {
+    console.log('jQuery Admin ready');
+
+    $('#adminLoginButton').on('click', CheckAdminInfo);
+
+    //Testdaten
+    $('#adminUsername').val('testadmin1');
+    $('#adminPasswort').val('hallo123');
+
+
+});
+
+function CheckAdminInfo() {
+
+    var input_username = $('#adminUsername').val();
+    var input_password = $('#adminPasswort').val();
+
+    console.log('Username: ' + input_username);
+    console.log('Password: ' + input_password);
+
+    const url = '/Admin/CheckAdminInfo';
+
+    $.post(url, {
+        username: input_username,
+        password: input_password
+    }).then(reply => {
+        console.log(`ServerReply CheckAdminInfo: ${reply}`);
+        if (reply === 'username_fail') {
+            $('#LoginError').html('User ist nicht vorhanden!');
+        } else if (reply === 'password_fail') {
+            $('#LoginError').html('Falsches Passwort!');
+        } else if (reply === 'ok') {
+            window.open('AdminOverview');
+            self.close();
+        }
+    });
+
+}
